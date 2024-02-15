e2cc51e35e19fcc7c2d6ce2e86479f0b599614fb
Romi Landau <romi7102@gmail.com>
2024-02-15 17:49:26 +0200

    clean up

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
 
     # ensure that index exists and if it doesn't exist create it
     existing_indexes = events.list_indexes()
-    index_name_to_check = "timestamp" 
+    index_name_to_check = "timestamp"
     index_exists = any(index["name"] == index_name_to_check for index in existing_indexes)
     if index_exists is False:
         events.create_index([("timestamp", ASCENDING)])
