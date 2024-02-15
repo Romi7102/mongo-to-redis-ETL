86e66f5fe333d75e3c77218f5e5f89bd45da3769
Romi Landau <romi7102@gmail.com>
2024-02-15 13:58:30 +0200

    improved redis query , now O(1)

--- main.py
+++ main.py
@@ -8,43 +8,37 @@ from pymongo import MongoClient
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT')
 REDIS_CLIENT = redis.Redis(REDIS_HOST , REDIS_PORT)
+REDIS_LAST = os.environ.get("REDIS_LAST")
 
 MONGO_CS = os.environ.get('MONGO_CS')
+MONGO_DB = os.environ.get('MONGO_DB')
+MONGO_COLLECTION = os.environ.get('MONGO_COLLECTION')
 MONGO_CLIENT = MongoClient(MONGO_CS)
 
-def latest_timestamp():
-    ret = None
-
-    for key in REDIS_CLIENT.keys('*'):
-        json_str = REDIS_CLIENT.get(key)
-        json_obj = json.loads(json_str)
-
-        timestamp_string = json_obj['timestamp']
-
-        if timestamp_string:
-            timestamp = datetime.strptime(timestamp_string, "%Y-%m-%d %H:%M:%S")
-
-            if ret is None or timestamp > ret:
-                ret = timestamp
-    return ret
-
+SLEEP = int(os.environ.get('SLEEP'))
 
 def main():
 
-    events = MONGO_CLIENT.testdb.events
-    latest_stamp = latest_timestamp()
+    events = MONGO_CLIENT[MONGO_DB][MONGO_COLLECTION]
+    latest_stamp = REDIS_CLIENT.get(REDIS_LAST)
+    print(latest_stamp)
     query = {}
+    if latest_stamp is not None:
+        latest_stamp = latest_stamp.decode('utf8')
+        latest_stamp = datetime.strptime(latest_stamp, "%Y-%m-%d %H:%M:%S")
+        query = {"timestamp": {"$gt": latest_stamp}}
 
     while True:
-        if latest_stamp:
-            query = {"timestamp": {"$gt": latest_stamp}}
-            print(latest_stamp)
+        
         for event in events.find(query):
             # remove mongo _id
             del event["_id"]
 
-            # change the latest timestamp 
-            latest_stamp = event["timestamp"]
+            # change the latest timestamp
+            if latest_stamp is None or event["timestamp"] > latest_stamp:
+                latest_stamp = event["timestamp"]
+                REDIS_CLIENT.set(REDIS_LAST, datetime.strftime(event["timestamp"], "%Y-%m-%d %H:%M:%S"))
+                query = {"timestamp": {"$gt": latest_stamp}}
 
             # format timestamp to string
             event["timestamp"] = datetime.strftime(event["timestamp"] ,"%Y-%m-%d %H:%M:%S")
@@ -54,7 +48,8 @@ def main():
             value = json.dumps(event).encode('utf8')
             REDIS_CLIENT.set(key, value)
 
-        sleep(5) #needs to be 30 , for convenience set to 5
+
+        sleep(SLEEP) #needs to be 30 , for convenience set to 5 
 
 if __name__ == '__main__':
     main()
